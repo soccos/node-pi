c114ef358cf4e09666ea011acbe3adb5261d94ff
pengxiguaa <pengxiguaa@gmail.com>
2018-04-08 23:53:24 +0800

    高亮

--- common/rander.js
+++ common/rander.js
@@ -2,13 +2,24 @@ const pug = require('pug');
 const hljs = require('highlight.js');
 const path = require('path');
 const moment = require('moment');
-const MD = require('markdown-it')();
+const MD = require('markdown-it')({
+  highlight: function (str, lang) {
+    if (lang && hljs.getLanguage(lang)) {
+      try {
+        return '<pre class="hljs"><code>' +
+          hljs.highlight(lang, str, true).value +
+          '</code></pre>';
+      } catch (__) {}
+    }
+    return '<pre class="hljs"><code>' + MD.utils.escapeHtml(str) + '</code></pre>';
+  }
+});
 moment.locale('zh-cn');
 const fromNow = (data) => {
   return moment(data).fromNow();
 };
 const markdown = (md) => {
-  return MD.render(md);
+  return MD.render(md, 'js');
 };
 const render = (template, data) => {
   const options = {
